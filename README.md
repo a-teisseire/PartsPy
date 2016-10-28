123586770dc502b608d0d2d92ec47abd10206c61
Anthony Teisseire <contact@a-teisseire.fr>
2016-10-28 17:34:25 +0200

    Base code and Readme

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
 # PartsPy
-Helpful tool for electronics hobbyist, DIYers, hackers, tinkerers...
+## What is PartsPy?
+PartsPy is a helpful tool written in Python for electronics hobbyist, DIYers, hackers, tinkerers, [insert your favorite term here].  
+It can accomplish various tasks:
+- Determine the value of color-bands resistors
+- Determine the value of SMD resistors
+- Determine the value of ceramic capacitors
+- Search for a part online with its description and datasheet *(Thanks to alldatasheets.com!)*
+- Allows simple conversion between different metric prefixes
+
+
+## How do I use it?
+**Syntax:** `$ ./parts.py command args...`
+
+PartsPy supports the following commands:
+- help : Displays the manual
+- res : Resistors
+- rsmd : SMD Resistors
+- cap : Capacitors
+- conv : Conversion
+- part : Part Search
+
+### Color bands resistors
+**Syntax:** `./parts.py res col1 col2 col3 [col4 [col5]]`  
+Gives ohm value of 3 to 5 bands resistors.
+
+**Example:** `./parts.py res bk bn bk bn"`  
+**Output:**
+
+    ========
+    Resistor
+    ========
+    Value: 1.0 Ohms 1%
+
+#### Color Codes
+| Color         | X  | XX    |
+|---------------|----|-------|
+| Black         | k  | bk    |
+| Brown         | n  | bn    |
+| Red           | r  | r     |
+| Orange        | o  | o     |
+| Yellow        | y  | y     |
+| Green         | n  | gn    |
+| Blue          | u  | bu    |
+| Violet/Purple | v  | v     |
+| Grey/Slay     | s  | sl,gy |
+| White         | w  | w     |
+| Silver        | si | si    |
+| Gold          | gd | gd    |
+
+### SMD Resistors
+**Syntax:** `./parts.py rsmd Code`  
+Gives ohm value of 3 and 4 digits SMD resistors (Yes it supports decimals).
+
+**Example:** `./parts.py rsmd 472`  
+**Output:**
+
+    ============
+    SMD Resistor
+    ============
+    Value: 4.7 kOhms
+
+### Capacitors
+**Syntax:** `./parts.py cap Code [VoltageCode ToleranceCode]`  
+Gives capacitance value of ceramic caps with support of voltage and tolerance.
+
+**Example:** `./parts.py cap 104`  
+**Output:**
+
+    =========
+    Capacitor
+    =========
+    Value: 100.0 nF
+
+**Example:** `./parts.py cap 104 1H B`  
+**Output:**
+
+    =========
+    Capacitor
+    =========
+    Value: 100.0 nF
+    Voltage: 50V
+    Tolerance: 0.1 pF
+    
+### Part Search
+**Syntax:** `./parts.py part PartName`  
+Searches online for descriptions and datasheets of a component. PartsPy lists 4 matching or closely matching components.  
+The results are parsed from alldatasheets.com. This website exists thanks to advertising and donations, please consider the latter.
+
+**Example:** `./parts.py part ATMEGA328P-pu`  
+**Output:**  
+
+    ===========
+    Part Search
+    ===========
+    Main Result:
+      Part: ATMEGA328P-PU
+      Description: 8-bit Microcontroller with 4/8/16/32K Bytes In-System Programmable Flash
+      Manufactuer: ATMEL Corporation 
+      Datasheet: http://www.alldatasheet.com/datasheet-pdf/pdf/392289/ATMEL/ATMEGA328P-PU.html
+    Result :
+      Part: ATmega328P-15AZ
+      Description: 8-bit Microcontroller with 32K Bytes In-System Programmable Flash
+      Datasheet: http://www.alldatasheet.com/datasheet-pdf/pdf/313558/ATMEL/ATmega328P-15AZ.html
+    Result :
+      Part: ATmega328P-15MZ
+      Description: 8-bit Microcontroller with 32K Bytes In-System Programmable Flash
+      Datasheet: http://www.alldatasheet.com/datasheet-pdf/pdf/313559/ATMEL/ATmega328P-15MZ.html
+    Result :
+      Part: ATMEGA328P-20AU
+      Description: 8-bit Atmel Microcontroller with 4/8/16K Bytes In-System Programmable Flash
+      Datasheet: http://www.alldatasheet.com/datasheet-pdf/pdf/520568/ATMEL/ATMEGA328P-20AU.html
+    Search results from: www.alldatasheet.com
+
+
+### Conversion
+**Syntax:** `./parts.py conv value[prefix] [toPrefix]  `  
+Converts a value from one unit prefix to an other one. Indicate 0 if you want to convert to the base unit.  
+
+**Example:** `./parts.py conv 2k 0`  
+**Output:**
+
+    Result : 2000.0
+
+**Example:** `./parts.py conv 222u n`  
+**Output:**
+
+    Result : 22000.0 n
+
+## License
+PartsPy is released under the MIT licensing terms. Check the LICENSE file in this repository for more information.
+
+Copyright (c) 2016 Anthony Teisseire
---
+++ parts.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python
+import sys, requests, re
+from bs4 import BeautifulSoup
+
+units = [
+    ('G', 9),
+    ('M', 6),
+    ('k', 3),
+    ('',  0),
+    ('m', -3),
+    ('u', -6),
+    ('n', -9),
+    ('p', -12)
+]
+
+def formatToUnit(val, toUnit=None):
+    toUnitValue = 0
+
+    if toUnit != None:
+        if toUnit == '0':
+            toUnit = '' #We don't want to display '0' as the prefix
+        else:
+            toUnitValue = dict(units)[toUnit]
+    else: #Find best suited unit
+        for p, u in units:
+            if val >= (10 ** u):
+                toUnit = p
+                toUnitValue = u
+                break
+
+    return "%s %s" % (str(float(val) / (10 ** toUnitValue)), toUnit)
+
+def usage(args):
+    print "==================="
+    print "Parts.py Usage Help"
+    print "==================="
+    print "Format: ./parts.py Type Values"
+    print "\nArgs:"
+    print "  Type :"
+    print "\thelp : Displays this information"
+    print "\tres : Color bands resistor"
+    print "\trsmd : SMD resistor"
+    print "\tcap : Capacitor"
+    print "\tconv : Unit Conversion"
+    print "\nColor bands resistors"
+    print "  Example: ./parts.py res bk bn bk bn"
+    print "  Gives ohm value of 3 to 5 bands resistors"
+    print "  Color Codes:"
+    print "\tBlack=bk,k ; Brown=bn,n ; Red=r ; Orange=o ; Yellow=y ; Green=gn,n ; Blue=bu,u ;"
+    print "\tViolet/Purple=v ; Grey/Slay=s,sl,gy ; White=w ; Silver=si ; Gold=gd ;"
+    print "\nSMD Resistors"
+    print "  Example: ./parts.py rsmd 102 ; ./parts.py rsmd 22R2"
+    print "  Gives ohm value of 3 and 4 digits SMD resistors"
+    print "\nCapacitors"
+    print "  Example: ./parts.py cap 104 ; ./parts.py cap 104 1H B"
+    print "  Gives capacitance value of ceramic caps with support of voltage and tolerance"
+    print "\nConversion"
+    print "  Example: ./parts.py conv 2000 k ; ./parts.py conv 222p n"
+    print "  Format: value[suffix] [toSuffix]"
+    print "  Converts a value from one unit prefix to an other one"
+    print "\nScript by Anthony Teisseire"
+
+def smd_resistor(args):
+    code = args[0].upper()
+    val = 0
+
+    if len(code) < 3 or len(code) > 4:
+        print "Bad SMD resistor code"
+        return
+
+    dec = code.find('R')
+    if dec > -1:
+        val = float(code.replace('R', '.'))
+    else:
+        val = int(code[:-1]) * (10 ** int(code[-1]))
+
+    print "============"
+    print "SMD Resistor"
+    print "============"
+    print "Value: %sOhms" % formatToUnit(val) + ('' if val > 1 else (" (%s)" % val))
+
+
+def resistor(bands):
+    band_val = {
+        'k': 0, 'bk': 0,
+        'n': 1, 'bn': 1,
+        'r': 2,
+        'o': 3,
+        'y': 4,
+        'g': 5, 'gn': 5,
+        'u': 6, 'bu': 6,
+        'v': 7,
+        's': 8, 'sl': 8, 'gy': 8, #gy is here for convienence
+        'w': 9,
+
+        #multipliers
+        'gd': -1,
+        'si': -2
+    }
+
+    tol_val = {
+        'n': 1, 'bn': 1,
+        'r': 2,
+        'g': 0.5, 'gn': 0.5,
+        'u': 0.25, 'bu': 0.25,
+        'v': 0.1,
+        's': 0.05, 'sl': 0.05, 'gy': 0.05, #gy is still here for convienence
+
+        'gd': 5,
+        'si': 10
+    }
+
+    valBands = 2 if len(bands) < 5 else 3
+
+    val = 0
+    tol = 20
+    for b in bands[:valBands]:
+        val = val * 10 + band_val[b.lower()]
+
+    val = val * (10 ** band_val[bands[valBands].lower()])
+
+    if len(bands) > 3:
+        tol = tol_val[bands[-1].lower()]
+    
+    print "========"
+    print "Resistor"
+    print "========"
+    print "Value: %sOhms %i%%" % (formatToUnit(val), tol)
+
+
+def capacitor(args):
+    voltage = {
+        '1h': 50,
+        '2a': 100,
+        '2t': 150,
+        '2d': 200,
+        '2e': 250,
+        '2g': 400,
+        '2j': 630
+    }
+
+    tol = {
+        'b': '0.1 pF',
+        'c': '0.25 pF',
+        'd': '0.5 pF',
+        'f': '1%',
+        'g': '2%',
+        'h': '3%',
+        'j': '5%',
+        'k': '10%',
+        'm': '20%',
+        'z': '+80%, -20%'
+    }
+
+    argc = len(args)
+    code = args[0]
+
+    val = int(code[:-1]) * (10 ** (-12 + int(code[-1]))) #values expressed in pF (-12)
+    
+    print "========="
+    print "Capacitor"
+    print "========="
+    print "Value: %sF" % (formatToUnit(val))
+    
+    if argc == 3:
+        print "Voltage: %iV" % voltage[args[1].lower()]
+        print "Tolerance: %s" % tol[args[2].lower()]
+
+def part_search(args):
+    name = args[0]
+    endpoint = 'http://www.alldatasheet.com/view.jsp?Searchword=%s'
+    html = requests.get(endpoint % name).text
+    
+    parsed = BeautifulSoup(html, "lxml")
+    
+    tags = parsed.find_all('tr', limit=4, id=re.compile("cell[0-9]{1,2}"))
+    tds = tags[0].find_all('td')
+
+    print "==========="
+    print "Part Search"
+    print "==========="
+    print "Main Result:"
+    print "  Part: %s" % tds[1].a.get_text().strip()
+    print "  Description: %s" % tds[3].string
+    print "  Manufactuer: %s " %  tds[0].br.next_sibling
+    print "  Datasheet: %s" % tds[1].a['href']
+
+    for tag in tags[1:]:
+        tds = tag.find_all('td')
+       
+        #print tds
+        if len(tds) > 3:
+            tds = tds[1:] #chomp manufacturer if it's there 
+
+        print "Result :"
+        print "  Part: %s" % tds[0].a.get_text().strip()
+        print "  Description: %s" % tds[2].string
+        print "  Datasheet: %s" % tds[0].a['href']
+
+    print "Search results from: www.alldatasheet.com"
+
+
+def unit_convert(args):
+    fromVal = args[0]
+    fromUnit = fromVal[-1]
+    toUnit = args[1]
+
+    if not fromUnit.isdigit():
+        fromUnitValue = dict(units)[fromUnit]
+        fromVal = fromVal[:-1]
+    else:
+        fromUnitValue = 0
+
+    print "Result : %s" % formatToUnit(float(fromVal) * (10 ** fromUnitValue), toUnit)
+
+
+parts_sw = {
+    'rsmd': smd_resistor,
+    'res': resistor,
+    'cap': capacitor,
+    'part': part_search,
+    'conv': unit_convert,
+    'help': usage
+}
+
+def main():
+    if len(sys.argv) < 2:
+        print "Not enough arguments"
+        usage()
+        sys.exit(2)
+
+    parts_sw.get(sys.argv[1], usage)(sys.argv[2:])
+
+if __name__ == "__main__":
+    main()
